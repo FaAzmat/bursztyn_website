7a6b1ceee217c15d893e807b2c37b30ab2e9ae92
FaAzmat <125387771+FaAzmat@users.noreply.github.com>
2023-06-26 12:06:45 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const handleMobileMenuButton = (() => {
-  const button = document.querySelector(".menu-button");
-
-  button.addEventListener("click", () => {
-    document.body.classList.toggle("show-menu");
-  });
-})();
